8c161344995e3e4dd3b570678763d0eb6918a44f
escobaradelaida <adelaidaescobar@oakland.edu>
2024-02-05 19:22:35 -0500

    Added function to reorder the letters and their frequencies

--- test.cpp
+++ test.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
 #include <cctype>
+#include <algorithm>
 
 using namespace std;
 
-void count_frequencies(const string& file_name)
+void count_frequencies(const string& file_name, int lowercase[], int uppercase[])
 {
     //setting up arrays for both lower and upper letters
     int lowercase[26] = {0};
@@ -49,6 +50,37 @@ void count_frequencies(const string& file_name)
     }
 }
 
+//sorting the frequencies by ascending...hopefully
+
+void sort_frequencies(int lowercase[], int uppercase[])
+{
+    // Sort lowercase letters by frequency
+    for (int i = 0; i < 25; ++i)
+    {
+        for (int j = i + 1; j < 26; ++j)
+        {
+            if (lowercase[j] > lowercase[i])
+            {
+                swap(lowercase[i], lowercase[j]);
+                swap(uppercase[i], uppercase[j]);
+            }
+        }
+    }
+
+    // Sort uppercase letters by frequency
+    for (int i = 0; i < 25; ++i)
+    {
+        for (int j = i + 1; j < 26; ++j)
+        {
+            if (uppercase[j] > uppercase[i])
+            {
+                swap(lowercase[i], lowercase[j]);
+                swap(uppercase[i], uppercase[j]);
+            }
+        }
+    }
+}
+
 int main()
 {
     string file_name = "Project02-input.txt";
@@ -56,6 +88,19 @@ int main()
     cin >> file_name;
 
     count_frequencies(file_name);
+    sort_frequencies(lowercase, uppercase);
+
+    cout << "Letters sorted by frequency: " << endl;
+    for (int i = 0; i < 26; ++i)
+    {
+        if (lowercase[i] > 0)
+            cout << static_cast<char>('a' + i) << ": " << lowercase[i] << endl;
+    }
+    for (int i = 0; i < 26; ++i)
+    {
+        if (uppercase[i] > 0)
+            cout << static_cast<char>('A' + i) << ": " << uppercase[i] << endl;
+    }
 
     return 0;
 }
